998f5411d5df0d3312be982d9179188b92ac6db1
Brayan David Henrriquez Vasquez (24122) <brayan.henrriquez@est.zamorano.edu>
2022-09-12 22:05:04 -0600

    lab02

---
+++ Lab02-cadena.py
@@ -0,0 +1,8 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+x = "hello, world"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
---
+++ valor-de-pago.py
@@ -0,0 +1,9 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+#
+horas= input("horas trabajadoras?")
+valorporhora= input("valor por hora?")
+
+# se utiliza la conversion de tipo a int
+# sino se hace la conversion existiera error porque trata de multiplicar strings
+#total = int(horas) * int(valorporhora)
+print (horas + valorporhora)
